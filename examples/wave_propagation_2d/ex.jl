9cd5bc98afa6517fce5ac0ad5125a9292684a6cb
Sarah Yvonne Greer <sygreer@mit.edu>
2021-09-04 21:20:24 +0000

    2d wave propagation/inversion working

--- examples/wave_propagation_2d/ex.jl
+++ examples/wave_propagation_2d/ex.jl
@@ -2,7 +2,9 @@
 # \frac{\partial ^2 u}{\partial t ^2} - c^2 [ \frac{\partial ^2 u}{\partial z^2} + \frac{\partial ^2 u}{\partial x^2}] + f = 0
 
 import DPFEHM
-using Printf 
+#using Printf 
+using Zygote
+import PyPlot
 ## FUNCTIONS ####################
 
 # generate source wavelet (Ricker wavelet)
@@ -11,18 +13,13 @@ function ricker(f, t; Ã=0)  # generate ricker wavelet
 end
 
 # get norm to generate objective function
-function getNorm(c, f, nz, nx, nt, ridx, ur)
-    usave = DPFEHM.getuIters(c, f, nz, nx, nt)
+function getNorm(c, f, nz, nx, nt, dt, ridx, ur)
+    usave = DPFEHM.getuIters(c, f, nz, nx, nt, dz, dx, dt)
     l2norm = 0
     for i=1:length(ridx)
         l2norm += sum((ur[i, :] .- usave[ridx[i], :]).^2)
     end
     return l2norm
-end
-
-# reindex from 2D to 1D array
-function linearIndex(iz, ix)
-    return (ix-1)*nz + iz
 end
 
 # plot shot record
@@ -71,26 +68,50 @@ c = reshape(c, nz * nx)
 
 # truth velocity model (scatterer in center of domain)
 ctrue = 2000 .*ones(nz, nx) # m/s
-ctrue[halfz, halfx] = 2800 # m/s
+#ctrue[halfz, halfx] = 2800 # m/s
+sz = 4 # scatterer size
+ctrue[halfz-sz:halfz+sz, halfx-sz:halfx+sz] .= 2500 # m/s
+#ctrue[halfz-sz-25:halfz+sz-25, halfx-sz:halfx+sz] .= 3200 # m/s
 ctrue = reshape(ctrue, nz * nx)
 
 # receiver locations along surface
 xloc = Array(1:nx)
 zloc = 5 .*ones(size(xloc))
-ridx = floor.(Int, linearIndex.(zloc, xloc))
+ridx = floor.(Int, DPFEHM.linearIndex.(nz.*ones(size(xloc)), zloc, xloc))
 
-## get wavefield in background medium
-#@time u = DPFEHM.getuIters(c, f, nz, nx, nt)
+# get wavefield in background medium (can comment out)
+@time u = DPFEHM.getuIters(c, f, nz, nx, nt, dz, dx, dt)
+fig, axs = PyPlot.subplots(2, 13, figsize=(30, 5))
+axs = permutedims(axs, [2, 1])
+j = 1
+#for i = 2:10:size(u, 2)
+for i = 2:20:size(u, 2)
+    global j
+    axs[j].imshow(reshape(u[:, i], nz, nx), cmap="seismic")
+    j += 1
+end
+fig.tight_layout()
+
 
 # get wavefield in truth velocity model
-@time utrue = DPFEHM.getuIters(ctrue, f, nz, nx, nt)
+@time utrue = DPFEHM.getuIters(ctrue, f, nz, nx, nt, dz, dx, dt)
+fig, axs = PyPlot.subplots(2, 13, figsize=(30, 5))
+axs = permutedims(axs, [2, 1])
+j = 1
+#for i = 2:10:size(u, 2)
+for i = 2:20:size(utrue, 2)
+    global j
+    axs[j].imshow(reshape(utrue[:, i], nz, nx), cmap="seismic")
+    j += 1
+end
+fig.tight_layout()
 
 # shot record (data recorded at receivers)
 shotRecord = utrue[ridx, :] # get the shot record
 
 # gradient functions
-g3(c, f) = getNorm(c, f, nz, nx, nt, ridx, shotRecord)
-g3(c) = getNorm(c, f, nz, nx, nt, ridx, shotRecord)
+g3(c, f) = getNorm(c, f, nz, nx, nt, dt, ridx, shotRecord)
+g3(c) = getNorm(c, f, nz, nx, nt, dt, ridx, shotRecord)
 
 # Full waveform inversion (with gradient descent)
 niter=10                            # number of iterations
@@ -100,9 +121,11 @@ stepLength=1e10 .*ones(niter)       # step length
 l2save = zeros(niter)               # save loss function at every iteration
 
 # update model
+#for i = 1:2 # for one iteration (aka RTM)
 for i = 1:niter-1
     l2save[i] = g3(csave[:, i])
+    @show i
     grad = Zygote.gradient(g3, csave[:, i])
     csave[:, i+1] = csave[:, i] .- stepLength[i].*reshape(grad[1], nz*nx) 
-    @printf("Done with iteration %i\n", i)
+    #@printf("Done with iteration %i\n", i)
 end
--- src/wave.jl
+++ src/wave.jl
@@ -1,16 +1,21 @@
-# 2D acoustic wave propagation with Clayton-Enguist absorbing boundary conditions
-# 4th order in space, 1st order in time
-@NonlinearEquations.equations exclude=(nz, nx) function wave2diter(c, f, u, uim1, uim2, nz, nx) # 1st order
+import Zygote
+
+function linearIndex(nz, iz, ix) 
+    return (ix-1)*nz + iz
+end
+
+@NonlinearEquations.equations exclude=(nz, nx) function wave2diter(c, f, u, uim1, uim2, nz, nx, nt, dz, dx, dt) # 1st order
     NonlinearEquations.setnumequations(nz*nx)
 
     ## TOP ROW #########################
+    # top row (x, not corners)
     iz = 1
     for ix=2:nx-1
-        v1 = c[linearIndex(1, ix)] * dt/dz
-        v2 = c[linearIndex(1, ix)] * dt/dx
-        diff1 = v1*(-(uim1[linearIndex(2, ix)] - uim1[linearIndex(1, ix)] - uim2[linearIndex(2, ix)] - uim2[linearIndex(1, ix)]))
-        diff2 = 0.5*v2^2*(uim1[linearIndex(1, ix-1)] -2*uim1[linearIndex(1, ix)] + uim1[linearIndex(1, ix+1)])
-        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(1, ix)] - 2*uim1[linearIndex(1, ix)] -diff1 - diff2)
+        v1 = c[linearIndex(nz, 1, ix)] * dt/dz
+        v2 = c[linearIndex(nz, 1, ix)] * dt/dx
+        diff1 = v1*(-(uim1[linearIndex(nz, 2, ix)] - uim1[linearIndex(nz, 1, ix)] - uim2[linearIndex(nz, 2, ix)] - uim2[linearIndex(nz, 1, ix)]))
+        diff2 = 0.5*v2^2*(uim1[linearIndex(nz, 1, ix-1)] -2*uim1[linearIndex(nz, 1, ix)] + uim1[linearIndex(nz, 1, ix+1)])
+        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(nz, 1, ix)] - 2*uim1[linearIndex(nz, 1, ix)] -diff1 - diff2)
 
     end
 
@@ -18,20 +23,21 @@
     # second to top row (x, not corners)
     iz = 2
     for ix=3:nx-2
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(2, ix-2)] +16*uim1[linearIndex(2, ix-1)] + -30*uim1[linearIndex(2, ix)] +16*uim1[linearIndex(2, ix+1)] - uim1[linearIndex(2, ix+2)])/dx^2/12*c[linearIndex(2, ix)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, 2, ix-2)] +16*uim1[linearIndex(nz, 2, ix-1)] + -30*uim1[linearIndex(nz, 2, ix)] +16*uim1[linearIndex(nz, 2, ix+1)] - uim1[linearIndex(nz, 2, ix+2)])/dx^2/12*c[linearIndex(nz, 2, ix)]^2)
     end
     # second from top row (z)
     for ix=1:nx
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(1, ix)] - 2*uim1[linearIndex(2, ix)] + uim1[linearIndex(3, ix)])/dz^2*c[linearIndex(2, ix)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(nz, 1, ix)] - 2*uim1[linearIndex(nz, 2, ix)] + uim1[linearIndex(nz, 3, ix)])/dz^2*c[linearIndex(nz, 2, ix)]^2)
     end
 
     ## BOTTOM ROW #########################
+
     for ix=2:nx-1
-        v1 = c[linearIndex(nz, ix)] * dt/dz
-        v2 = c[linearIndex(nz, ix)] * dt/dx
-        diff1 = v1*(-(uim1[linearIndex(nz, ix)] - uim1[linearIndex(nz-1, ix)] - uim2[linearIndex(nz, ix)] - uim2[linearIndex(nz-1, ix)]))
-        diff2 = 0.5*v2^2*(uim1[linearIndex(nz, ix-1)] -2*uim1[linearIndex(nz, ix)] + uim1[linearIndex(nz, ix+1)])
-        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(nz, ix)] - 2*uim1[linearIndex(nz, ix)] -diff1 - diff2)
+        v1 = c[linearIndex(nz, nz, ix)] * dt/dz
+        v2 = c[linearIndex(nz, nz, ix)] * dt/dx
+        diff1 = v1*(-(uim1[linearIndex(nz, nz, ix)] - uim1[linearIndex(nz, nz-1, ix)] - uim2[linearIndex(nz, nz, ix)] - uim2[linearIndex(nz, nz-1, ix)]))
+        diff2 = 0.5*v2^2*(uim1[linearIndex(nz, nz, ix-1)] -2*uim1[linearIndex(nz, nz, ix)] + uim1[linearIndex(nz, nz, ix+1)])
+        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(nz, nz, ix)] - 2*uim1[linearIndex(nz, nz, ix)] -diff1 - diff2)
 
     end
 
@@ -39,21 +45,21 @@
     # second to bottom row (x, not corners)
     iz = nz - 1
     for ix=3:nx-2
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz-1, ix-2)] +16*uim1[linearIndex(nz-1, ix-1)] + -30*uim1[linearIndex(nz-1, ix)] +16*uim1[linearIndex(nz-1, ix+1)] - uim1[linearIndex(nz-1, ix+2)])/dx^2/12*c[linearIndex(nz-1, ix)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, nz-1, ix-2)] +16*uim1[linearIndex(nz, nz-1, ix-1)] + -30*uim1[linearIndex(nz, nz-1, ix)] +16*uim1[linearIndex(nz, nz-1, ix+1)] - uim1[linearIndex(nz, nz-1, ix+2)])/dx^2/12*c[linearIndex(nz, nz-1, ix)]^2)
     end
     # second from bottom row (z)
     for ix=1:nx
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(nz, ix)] - 2*uim1[linearIndex(nz-1, ix)] + uim1[linearIndex(nz-2, ix)])/dz^2*c[linearIndex(nz-1, ix)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(nz, nz, ix)] - 2*uim1[linearIndex(nz, nz-1, ix)] + uim1[linearIndex(nz, nz-2, ix)])/dz^2*c[linearIndex(nz, nz-1, ix)]^2)
     end
 
     # LEFT COLUMN ####################
     ix = 1
     for iz=2:nz-1
-        v1 = c[linearIndex(iz, 1)] * dt/dz
-        v2 = c[linearIndex(iz, 1)] * dt/dx
-        diff1 = 0.5*v1^2*(uim1[linearIndex(iz-1, 1)] -2*uim1[linearIndex(iz, 1)] + uim1[linearIndex(iz+1, 1)])
-        diff2 = v2*((uim1[linearIndex(iz, 2)] - uim1[linearIndex(iz, 1)] - uim2[linearIndex(iz, 2)] - uim2[linearIndex(iz, 1)]))
-        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(iz, 1)] - 2*uim1[linearIndex(iz, 1)] -diff1 - diff2)
+        v1 = c[linearIndex(nz, iz, 1)] * dt/dz
+        v2 = c[linearIndex(nz, iz, 1)] * dt/dx
+        diff1 = 0.5*v1^2*(uim1[linearIndex(nz, iz-1, 1)] -2*uim1[linearIndex(nz, iz, 1)] + uim1[linearIndex(nz, iz+1, 1)])
+        diff2 = v2*((uim1[linearIndex(nz, iz, 2)] - uim1[linearIndex(nz, iz, 1)] - uim2[linearIndex(nz, iz, 2)] - uim2[linearIndex(nz, iz, 1)]))
+        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(nz, iz, 1)] - 2*uim1[linearIndex(nz, iz, 1)] -diff1 - diff2)
 
     end
 
@@ -61,22 +67,22 @@
     # second to left edge (x)
     ix = 2
     for iz=1:nz
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(iz, 1)] - 2*uim1[linearIndex(iz, 2)] + uim1[linearIndex(iz, 3)])/dx^2*c[linearIndex(iz, 2)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(nz, iz, 1)] - 2*uim1[linearIndex(nz, iz, 2)] + uim1[linearIndex(nz, iz, 3)])/dx^2*c[linearIndex(nz, iz, 2)]^2)
     end
 
     # second to left edge (z, not corners)
     for iz=3:nz-2
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(iz-2, 2)] +16*uim1[linearIndex(iz-1, 2)] + -30*uim1[linearIndex(iz, 2)] +16*uim1[linearIndex(iz+1, 2)] - uim1[linearIndex(iz+2, 2)])/dz^2/12*c[linearIndex(iz, 2)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, iz-2, 2)] +16*uim1[linearIndex(nz, iz-1, 2)] + -30*uim1[linearIndex(nz, iz, 2)] +16*uim1[linearIndex(nz, iz+1, 2)] - uim1[linearIndex(nz, iz+2, 2)])/dz^2/12*c[linearIndex(nz, iz, 2)]^2)
     end
 
     # RIGHT COLUMN ####################
     ix = nx
     for iz=2:nz-1
-        v1 = c[linearIndex(iz, nx)] * dt/dz
-        v2 = c[linearIndex(iz, nx)] * dt/dx
-        diff1 = 0.5*v1^2*(uim1[linearIndex(iz-1, nx)] -2*uim1[linearIndex(iz, nx)] + uim1[linearIndex(iz+1, nx)])
-        diff2 = v2*(-(uim1[linearIndex(iz, nx)] - uim1[linearIndex(iz, nx-1)] - uim2[linearIndex(iz, nx)] - uim2[linearIndex(iz, nx-1)]))
-        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(iz, nx)] - 2*uim1[linearIndex(iz, nx)] -diff1 - diff2)
+        v1 = c[linearIndex(nz, iz, nx)] * dt/dz
+        v2 = c[linearIndex(nz, iz, nx)] * dt/dx
+        diff1 = 0.5*v1^2*(uim1[linearIndex(nz, iz-1, nx)] -2*uim1[linearIndex(nz, iz, nx)] + uim1[linearIndex(nz, iz+1, nx)])
+        diff2 = v2*(-(uim1[linearIndex(nz, iz, nx)] - uim1[linearIndex(nz, iz, nx-1)] - uim2[linearIndex(nz, iz, nx)] - uim2[linearIndex(nz, iz, nx-1)]))
+        NonlinearEquations.addterm((ix-1)*nz+iz, u[linearIndex(nz, iz, nx)] - 2*uim1[linearIndex(nz, iz, nx)] -diff1 - diff2)
 
     end
 
@@ -84,95 +90,72 @@
     # second to right edge (x)
     ix = nx - 1
     for iz=1:nz
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(iz, nx)] - 2*uim1[linearIndex(iz, nx-1)] + uim1[linearIndex(iz, nx-2)])/dx^2*c[linearIndex(iz, nx-1)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(uim1[linearIndex(nz, iz, nx)] - 2*uim1[linearIndex(nz, iz, nx-1)] + uim1[linearIndex(nz, iz, nx-2)])/dx^2*c[linearIndex(nz, iz, nx-1)]^2)
     end
 
     # second to right edge (z, not corners)
     for iz=3:nz-2
-        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(iz-2, nx-1)] +16*uim1[linearIndex(iz-1, nx-1)] + -30*uim1[linearIndex(iz, nx-1)] +16*uim1[linearIndex(iz+1, nx-1)] - uim1[linearIndex(iz+2, nx-1)])/dz^2/12*c[linearIndex(iz, nx-1)]^2)
+        NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, iz-2, nx-1)] +16*uim1[linearIndex(nz, iz-1, nx-1)] + -30*uim1[linearIndex(nz, iz, nx-1)] +16*uim1[linearIndex(nz, iz+1, nx-1)] - uim1[linearIndex(nz, iz+2, nx-1)])/dz^2/12*c[linearIndex(nz, iz, nx-1)]^2)
     end
 
     # CENTER ####################
     for iz=3:nz-2
         for ix=3:nx-2
-            NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(iz-2, ix)] +16*uim1[linearIndex(iz-1, ix)] + -30*uim1[linearIndex(iz, ix)] +16*uim1[linearIndex(iz+1, ix)] - uim1[linearIndex(iz+2, ix)])/(dz^2*12)*c[linearIndex(iz, ix)]^2) # z
-            NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(iz, ix-2)] +16*uim1[linearIndex(iz, ix-1)] + -30*uim1[linearIndex(iz, ix)] +16*uim1[linearIndex(iz, ix+1)] - uim1[linearIndex(iz, ix+2)])/(dx^2*12)*c[linearIndex(iz, ix)]^2) # x
+            NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, iz-2, ix)] +16*uim1[linearIndex(nz, iz-1, ix)] + -30*uim1[linearIndex(nz, iz, ix)] +16*uim1[linearIndex(nz, iz+1, ix)] - uim1[linearIndex(nz, iz+2, ix)])/(dz^2*12)*c[linearIndex(nz, iz, ix)]^2) # z
+            NonlinearEquations.addterm((ix-1)*nz + iz, -1*(-uim1[linearIndex(nz, iz, ix-2)] +16*uim1[linearIndex(nz, iz, ix-1)] + -30*uim1[linearIndex(nz, iz, ix)] +16*uim1[linearIndex(nz, iz, ix+1)] - uim1[linearIndex(nz, iz, ix+2)])/(dx^2*12)*c[linearIndex(nz, iz, ix)]^2) # x
         end
     end
 
-    # TIME DERIVATIVE AND SOURCE ####################
+    ################################################################################
+    # time derivative and source ####################
     for iz=1:nz
         for ix=1:nx
             # forcing term
-            NonlinearEquations.addterm((ix-1)*nz + iz, f[linearIndex(iz, ix)])
+            NonlinearEquations.addterm((ix-1)*nz + iz, f[linearIndex(nz, iz, ix)])
 
             # 1st order time derivative
-            NonlinearEquations.addterm((ix-1)*nz + iz, (u[linearIndex(iz, ix)] - 2*uim1[linearIndex(iz, ix)] + uim2[linearIndex(iz, ix)])/dt^2 )
+            NonlinearEquations.addterm((ix-1)*nz + iz, (u[linearIndex(nz, iz, ix)] - 2*uim1[linearIndex(nz, iz, ix)] + uim2[linearIndex(nz, iz, ix)])/dt^2 )
         end
     end
 end
 
-# forward model one iteration
-function uoneiter(ui, uim1, uim2, c, f) # 1st order, one iteration of wave propagation
-    return ui - dt^2 * wave2diter_residuals(c, f, ui, uim1, uim2, nz, nx)
+
+## WAVE PROPAGATION ####################
+function uoneiter(ui, uim1, uim2, c, f, nz, nx, nt, dz, dx, dt) # 1st order, one iteration of wave propagation
+    return ui - dt^2 * wave2diter_residuals(c, f, ui, uim1, uim2, nz, nx, nt, dz, dx, dt)
 end
 
-# forward model multiple iterations
-function getuIters(cval, f, nz, nx, nt)
+# multiple iterations
+function getuIters(cval, f, nz, nx, nt, dz, dx, dt)
     u = zeros(nz*nx, nt + 2)
     for i=3:nt + 2
-        u[:, i] = uoneiter(zeros(size(u, 1)), u[:, i - 1], u[:, i - 2], cval, f[:, i - 2])
-        if mod(i, 10) == 0
-           @printf("Iteration %i\n", i)
-        end
+        u[:, i] = uoneiter(zeros(size(u, 1)), u[:, i - 1], u[:, i - 2], cval, f[:, i - 2], nz, nx, nt, dz, dx, dt)
+        #if mod(i, 10) == 0
+        #   @printf("Iteration %i\n", i)
+        #end
     end
     return u
 end
 
-# pullback for one iteration
-function make_wave2diter_pullback(args...)
-    function wave2diter_pullback(delta)
-		retval = (ChainRulesCore.NO_FIELDS,#function
-                @ChainRulesCore.thunk(wave2diter_c(args...)' * delta),#c
-				@ChainRulesCore.thunk(wave2diter_f(args...)' * delta),#f
-				@ChainRulesCore.thunk(wave2diter_u(args...)' * delta),#u
-				@ChainRulesCore.thunk(wave2diter_uim1(args...)' * delta),#uim1
-				@ChainRulesCore.thunk(wave2diter_uim2(args...)' * delta),#uim2
-                ChainRulesCore.NO_FIELDS,#nz
-                ChainRulesCore.NO_FIELDS)#nx
-        return retval
-    end
-    return wave2diter_pullback
-end
-
-# rrule for one iteration
-function ChainRulesCore.rrule(::typeof(wave2diter_residuals), c, f, u, uim1, uim2, nz, nx)
-    args = (c, f, u, uim1, uim2, nz, nx)
-    residuals = wave2diter_residuals(args...)
-    pullback = make_wave2diter_pullback(args...)
-    return residuals, pullback
-end
-
-# rrule rule for multiple iterations
-function ChainRulesCore.rrule(::typeof(getuIters), c, f, nz, nx, nt)
-    u = getuIters(c, f, nz, nx, nt)
+function ChainRulesCore.rrule(::typeof(getuIters), c, f, nz, nx, nt, dz, dx, dt)
+    u = getuIters(c, f, nz, nx, nt, dz, dx, dt)
     return u, delta->begin
         ds = []
         #last iteration
-        _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, size(u, 2) - 1], u[:, size(u, 2) - 2], c, f[:, end])
+        _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, size(u, 2) - 1], u[:, size(u, 2) - 2], c, f[:, end], nz, nx, nt, dz, dx, dt)
         d1, d2, d3, d4, d5 = pb(delta[:, size(u, 2)])
         push!(ds, (d1, d2, d3))
         dc = d4
         df = hcat(d5)
         #second to last iteration
-        _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, size(u, 2) - 2], u[:, size(u, 2) - 3], c, f[:, end - 1])
+        _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, size(u, 2) - 2], u[:, size(u, 2) - 3], c, f[:, end - 1], nz, nx, nt, dz, dx, dt)
         d1, d2, d3, d4, d5 = pb(delta[:, size(u, 2) - 1] + ds[1][2])
         push!(ds, (d1, d2, d3))
         dc += d4
         df = hcat(d5, df)
         #now do the general iteration
         for i = size(u, 2) - 2:-1:3
-            _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, i - 1], u[:, i - 2], c, f[:, i - 2])
+            _, pb = Zygote.pullback(uoneiter, zeros(size(u, 1)), u[:, i - 1], u[:, i - 2], c, f[:, i - 2], nz, nx, nt, dz, dx, dt)
             d1, d2, d3, d4, d5 = pb(delta[:, i] + ds[2][2] + ds[1][3])
             dc += d4
             df = hcat(d5, df)
@@ -184,6 +167,38 @@ function ChainRulesCore.rrule(::typeof(getuIters), c, f, nz, nx, nt)
                 df,
                 ChainRulesCore.NO_FIELDS,#nz
                 ChainRulesCore.NO_FIELDS,#nx
-                ChainRulesCore.NO_FIELDS)#nt
+                ChainRulesCore.NO_FIELDS,#nt
+                ChainRulesCore.NO_FIELDS,#dz
+                ChainRulesCore.NO_FIELDS,#dx
+                ChainRulesCore.NO_FIELDS)#dt
     end
 end
+
+## PULLBACK FUNCTIONS ####################
+function make_wave2diter_pullback(args...)
+    function wave2diter_pullback(delta)
+		retval = (ChainRulesCore.NO_FIELDS,#function
+                @ChainRulesCore.thunk(wave2diter_c(args...)' * delta),#c
+				@ChainRulesCore.thunk(wave2diter_f(args...)' * delta),#f
+				@ChainRulesCore.thunk(wave2diter_u(args...)' * delta),#u
+				@ChainRulesCore.thunk(wave2diter_uim1(args...)' * delta),#uim1
+				@ChainRulesCore.thunk(wave2diter_uim2(args...)' * delta),#uim2
+                ChainRulesCore.NO_FIELDS,#nz
+                ChainRulesCore.NO_FIELDS,#nx
+                ChainRulesCore.NO_FIELDS,#nt
+                ChainRulesCore.NO_FIELDS,#dz
+                ChainRulesCore.NO_FIELDS,#dx
+                ChainRulesCore.NO_FIELDS)#dt
+        return retval
+    end
+    return wave2diter_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(wave2diter_residuals), c, f, u, uim1, uim2, nz, nx, nt, dz, dx, dt)
+    args = (c, f, u, uim1, uim2, nz, nx, nt, dz, dx, dt)
+    residuals = wave2diter_residuals(args...)
+    pullback = make_wave2diter_pullback(args...)
+    return residuals, pullback
+end
+
+
