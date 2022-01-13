c935e4bb5adde0f1650c0a639ab3d5cdee3c16b9
Daniel O'Malley <omalled@lanl.gov>
2022-01-13 13:26:51 -0700

    updates for changes in AlgebraicMultigrid

--- src/groundwater.jl
+++ src/groundwater.jl
@@ -22,7 +22,7 @@ end
 
 function amg_solver(A, b; kwargs...)
 	ml = AlgebraicMultigrid.ruge_stuben(A)
-	hfree = AlgebraicMultigrid.solve(ml, b; kwargs...)
+	hfree = AlgebraicMultigrid._solve(ml, b; kwargs...)
 	return hfree
 end
 
@@ -38,8 +38,6 @@ function groundwater_steadystate(Ks, neighbors, areasoverlengths, dirichletnodes
 	b = -DPFEHM.groundwater_residuals(args...)
 	A = DPFEHM.groundwater_h(args...)
 	hfree = linear_solver(A, b; kwargs...)
-	#ml = AlgebraicMultigrid.ruge_stuben(A)
-	#hfree = AlgebraicMultigrid.solve(ml, b; kwargs...)
 	h = map(i->isfreenode[i] ? hfree[nodei2freenodei[i]] : dirichleths[i], 1:length(Qs))
 	return h
 end
@@ -50,12 +48,12 @@ function ChainRulesCore.rrule(::typeof(groundwater_steadystate), Ks, neighbors, 
 	b = -DPFEHM.groundwater_residuals(args_noh...)
 	A = DPFEHM.groundwater_h(args_noh...)
 	ml = AlgebraicMultigrid.ruge_stuben(A)
-	hfree = AlgebraicMultigrid.solve(ml, b; kwargs...)
+	hfree = AlgebraicMultigrid._solve(ml, b; kwargs...)
 	h = map(i->isfreenode[i] ? hfree[nodei2freenodei[i]] : dirichleths[i], 1:length(Qs))
 	function pullback(delta)
 		args = (hfree, Ks, neighbors, areasoverlengths, dirichletnodes, dirichleths, Qs, ones(length(Qs)), ones(length(Qs)))
 		ml_A_prime = AlgebraicMultigrid.ruge_stuben(SparseArrays.SparseMatrixCSC(A'))
-		lambda = AlgebraicMultigrid.solve(ml_A_prime, delta[isfreenode]; kwargs...)
+		lambda = AlgebraicMultigrid._solve(ml_A_prime, delta[isfreenode]; kwargs...)
 		gw_Ks = groundwater_Ks(args...)
 		gw_dirichleths = groundwater_dirichleths(args...)
 		gw_Qs = groundwater_Qs(args...)
